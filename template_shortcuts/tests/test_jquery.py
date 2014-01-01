ef62c7f99460ee2608ded9b7747ccd44e9f78049
Andrei Coman <comandrei@gmail.com>
2014-01-01 20:17:25 +0200

    Support passing in a jquery version

--- template_shortcuts/tests/test_jquery.py
+++ template_shortcuts/tests/test_jquery.py
@@ -1,13 +1,16 @@
 import unittest
 
+from django import template
+
 from template_shortcuts.tests.helpers import render_to_string
 
 
 class JQueryTest(unittest.TestCase):
 
-    def test_jquery_latest(self):
-        rendered = render_to_string("{% jquery %}")
-        self.assertIn("jquery.min.js", rendered)
+    def test_jquery_version_required(self):
+        with self.assertRaises(template.TemplateSyntaxError):
+            render_to_string("{% jquery %}")
 
     def test_jquery_certain_version(self):
-        pass
+        rendered = render_to_string("{% jquery '1.7.2' %}")
+        self.assertIn("1.7.2/jquery.min.js", rendered)
