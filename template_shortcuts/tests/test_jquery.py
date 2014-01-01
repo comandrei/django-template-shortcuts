7fffad929df2709f21d181087de5206efc6ebf2b
Andrei Coman <comandrei@gmail.com>
2014-01-01 19:32:09 +0200

    Update compatible Django versions

--- template_shortcuts/tests/test_jquery.py
+++ template_shortcuts/tests/test_jquery.py
@@ -8,3 +8,6 @@ class JQueryTest(unittest.TestCase):
     def test_jquery_latest(self):
         rendered = render_to_string("{% jquery %}")
         self.assertIn("jquery.min.js", rendered)
+
+    def test_jquery_certain_version(self):
+        pass
