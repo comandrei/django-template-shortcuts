69dcce25a4accf1c910a7d273c6b8510603bda2a
Andrei Coman <comandrei@gmail.com>
2014-01-02 12:24:20 +0200

    Add test

--- template_shortcuts/tests/test_jquery.py
+++ template_shortcuts/tests/test_jquery.py
@@ -14,3 +14,10 @@ class JQueryTest(unittest.TestCase):
     def test_jquery_certain_version(self):
         rendered = render_to_string("{% jquery '1.7.2' %}")
         self.assertIn("1.7.2/jquery.min.js", rendered)
+
+
+class AngularTestCase(unittest.TestCase):
+
+    def test_angular_certain_version(self):
+        rendered = render_to_string("{% angular '1.2.5' %}")
+        self.assertIn("1.2.5/angular.min.js", rendered)
