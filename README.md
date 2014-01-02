4e71ce96d2716dc9e476d14c39c03f6a42a2d49b
Andrei Coman <comandrei@gmail.com>
2014-01-02 13:34:49 +0200

    More metadata for pypi

--- README.md
+++ README.md
@@ -3,4 +3,42 @@ django-template-shortcuts
 
 [![Build Status](https://travis-ci.org/comandrei/django-template-shortcuts.png)](https://travis-ci.org/comandrei/django-template-shortcuts)
 
-Reusable Django templatetags 
+Reusable Django templatetags
+
+
+
+
+
+## Index
+
+- [Instalation](#instalation)
+- [Contributing](#contributing)
+- [Usage](#usage)
+ - [Tags](#tags)
+ - [Filters](#filters)
+
+## Instalation
+
+Via pip:
+```bash
+    pip install django-template-utils
+```
+Via easy_install:
+```bash
+    easy_install django-template-utils
+```
+Clonning the project:
+```bash
+    $ git clone git://github.com/gerardo-orozco/django-template-utils.git
+    $ python django-template-utils/setup.py install
+```
+
+Add the app to your istalled apps:
+```python
+    INSTALLED_APPS = (
+        ...
+        'template_shortcuts',
+        ...
+    )
+	TEMPLATE_CDN_PROVIDER = "template_shortcuts.provides.google.Google"
+```
--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
+import os.path
 from setuptools import setup, find_packages
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
 setup(
-    name = "django-template-shortcuts",
-    version = "0.1",
-    packages = find_packages(),
+    name="django-template-shortcuts",
+    version="0.1",
+    packages=find_packages(),
     author="Andrei Coman",
     author_email="comandrei@gmail.com",
-    install_requires=["django>=1.2.7"],
-    url="https://github.com/comandrei/django-template-shortcuts/"
+    install_requires=["django>=1.3"],
+    url="https://github.com/comandrei/django-template-shortcuts/",
+    license='BSD',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
 )
